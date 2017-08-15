66b2bce7728db7ed1219c9a7bb34251856c24cc2
p1729 <pankajkumar1729@gmail.com>
2017-08-15 16:23:03 +0530

    mod1 solution

---
+++ mod1_solution/scripts/app.js
@@ -0,0 +1,36 @@
+(function() {
+
+angular.module("LunchCheck",[])
+
+.controller("LunchCheckController",LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+	$scope.lunchMenu = "";
+	
+	$scope.menuCheck = function() {
+		
+		var self = this;
+		var items = self.lunchMenu.split(",");
+		var pureItems = items.filter(function(item) {
+			console.log(item.trim().length);
+			return item.trim().length > 0;
+		});
+		var noOfItems = pureItems.length;
+		console.log(items,pureItems);
+
+		if(self.lunchMenu && noOfItems) {			
+			if(noOfItems <= 3 ) {
+				self.message = "Enjoy!";
+			} else {
+				self.message = "Too much!";
+			}		
+		} else {
+			self.message = "Please enter data first";
+		}
+	};
+};
+
+
+})();
